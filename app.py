9826494a9a25e8876017d83f5dc57f9ede354fec
OrnMichal <ornmichal@gmail.com>
2021-05-03 14:50:36 +0200

    com10

--- app.py
+++ app.py
@@ -6,26 +6,14 @@ from flask import abort, redirect, url_for, make_response
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
-
-@app.route('/error_not_found')
-def error_not_found():
-    response = make_response(render_template('template.html', name='ERROR 404'), 404)
-    response.headers['X-Something'] = 'A value'
-    return response
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
+    return redirect(url_for('about.html'))
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
