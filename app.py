de2678d101498e30e916722e45ddb93546957999
OrnMichal <ornmichal@gmail.com>
2021-05-03 15:49:27 +0200

    com11

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import abort, redirect, url_for, make_response
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app=Flask(__name__)
 
+#add pages here
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return redirect(url_for('about.html'))
-
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)    
