6b3a0b2ae388cd564173b09b4b38d06943b835c3
OrnMichal <ornmichal@gmail.com>
2021-05-03 15:57:58 +0200

    com11

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')   
+
+@app.route('/contact')
+def about():
+    return render_template('contact.html') 
+    
+@app.route('/gallery')
+def about():
+    return render_template('gallery.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)    
